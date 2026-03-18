df4454c62d28f83415970895370e9ba0f81182dc
Famanias <haratayo@gmail.com>
2026-03-18 22:51:22 +0900

    update plugins

--- server/main.py
+++ server/main.py
@@ -267,7 +267,7 @@ def start_translator_client_proc(host: str, port: int, nonce: str, params: Names
     base_path = os.path.dirname(os.path.abspath(__file__))
     parent = os.path.dirname(base_path)
     proc = subprocess.Popen(cmds, cwd=parent)
-    executor_instances.register(ExecutorInstance(ip=host, port=port))
+    executor_instances.register(ExecutorInstance(ip=host, port=port, nonce=nonce))
 
     def handle_exit_signals(signal, frame):
         proc.terminate()
